f673eb08294e3a301106008cb93f87bbfcf38fd4
Kun He <kun.he@sjsu.edu>
2019-03-11 01:16:46 -0700

    clean up and optimize

--- multi_archive_extractor/extractor.py
+++ multi_archive_extractor/extractor.py
@@ -1,35 +1,42 @@
 """
 Handles extraction for multiple archives
 """
-from subprocess import Popen
-import subprocess
+from subprocess import Popen, PIPE
 from shutil import rmtree
 import multiprocessing
+import os
 
 
 def extract(archive, pwd_list=None):
-    print('active ', archive)
     success = False
+    out_path = os.path.splitext(archive)[0]
+
     for pwd in pwd_list:
         if success:
             break
-        command = f'7z x {archive} -p{pwd} -y -o{archive.split(".")[-2]}'
-        p = Popen(command, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        command = f'7z x {archive} -p{pwd} -y -o{out_path}'
+        p = Popen(command, stderr=PIPE, stdout=PIPE)
+
         p.communicate()
         if p.returncode == 0:
             success = True
 
+    print(f'Success: {archive}' if success else f'Failed:  {archive}')
+
     # deleted empty files and directory
     if not success:
-        rmtree(archive.split(".")[-2], ignore_errors=True)
+        rmtree(out_path, ignore_errors=True)
 
     return success
 
 
 def mp_extraction(file_queue, pwd):
-    print(pwd)
-    print(file_queue)
-    map_arg = [(file, pwd) for file in file_queue]
+    map_args = [(file, pwd) for file in file_queue]
     with multiprocessing.Pool() as pool:
-        pool.starmap(extract, map_arg)
+        output = pool.starmap(extract, map_args)
+        success_count = 0
+        for stat in output:
+            if stat:
+                success_count += 1
+    print(f'\n{success_count} out of {len(file_queue)} succeeded')
 
